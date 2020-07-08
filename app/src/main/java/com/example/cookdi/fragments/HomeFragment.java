550e29d22c4047c8b66954bd6ca535db939f49c4
Ngô Việt Thắng <derekzohar@gmail.com>
2020-07-08 22:08:57 +0700

    tab view
    basic

---
+++ app/src/main/java/com/example/cookdi/fragments/HomeFragment.java
@@ -0,0 +1,33 @@
+package com.example.cookdi.fragments;
+
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.cookdi.R;
+import com.google.android.material.appbar.AppBarLayout;
+
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+}
