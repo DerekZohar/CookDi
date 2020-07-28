a8486aad461927a9177b15eb211161cc14ddb73e
DucMinhCao <minhduc.itus@gmail.com>
2020-07-28 20:01:07 +0700

    Fixed

--- app/src/main/java/com/example/cookdi/search/SearchActivity.java
+++ app/src/main/java/com/example/cookdi/search/SearchActivity.java
@@ -1,12 +1,15 @@
 package com.example.cookdi.search;
 
 import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,6 +18,8 @@ import com.example.cookdi.main.MainActivity;
 import com.veinhorn.tagview.TagView;
 
 public class SearchActivity extends AppCompatActivity {
+    TextView typeTextView;
+    TextView ingredientTextView;
     EditText searchEditText;
     TagView tagViewVegetable;
     TagView tagViewSoup;
@@ -29,6 +34,22 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        typeTextView = findViewById(R.id.typeTextView);
+        typeTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                typeTextView.setClickable(false);
+            }
+        });
+
+        ingredientTextView = findViewById(R.id.ingredientTextView);
+        ingredientTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ingredientTextView.setClickable(false);
+            }
+        });
 
         searchEditText = findViewById(R.id.editTextSearchView);
         tagViewVegetable = findViewById(R.id.tagViewVegetable);
@@ -97,10 +118,17 @@ public class SearchActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        final Drawable upArrow = getResources().getDrawable(R.drawable.abc_ic_ab_back_material);
+        upArrow.setColorFilter(getResources().getColor(R.color.white), PorterDuff.Mode.SRC_ATOP);
     }
 
     public void hideKeyBoard() {
-        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
-        imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        try {
+            InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        } catch (Exception e) {
+
+        }
     }
 }
