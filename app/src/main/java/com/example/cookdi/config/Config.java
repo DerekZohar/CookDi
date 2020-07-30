4b747db7abd9dbd3e2f64e58b12550c2db6c2a2e
Trung Dang <2017.12ch.34.dangductrung@gmail.com>
2020-07-30 23:47:09 +0700

    edit config base url

--- app/src/main/java/com/example/cookdi/config/Config.java
+++ app/src/main/java/com/example/cookdi/config/Config.java
@@ -1,10 +1,6 @@
 package com.example.cookdi.config;
 
 public class Config {
-    
-//    public static String BASE_URL = "http://localhost:3000";
-
-    //Using this base url for emulator using
-    public static String BASE_URL = "http://10.0.2.2:3000";
+    public static String BASE_URL = "https://cookdi.herokuapp.com";
     public static int LOG_ROUND_SALT = 12;
 }
