ec139f3f9278185ac5ad58a53389e1a868495b57
DucMinhCao <minhduc.itus@gmail.com>
2020-08-05 14:53:04 +0700

    Test

---
+++ app/src/main/java/com/example/cookdi/upload/UploadActivity.java
@@ -0,0 +1,32 @@
+package com.example.cookdi.upload;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cookdi.R;
+import com.example.cookdi.main.MainActivity;
+
+public class UploadActivity  extends AppCompatActivity {
+
+    ImageButton backButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload);
+
+        backButton = findViewById(R.id.back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(UploadActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
