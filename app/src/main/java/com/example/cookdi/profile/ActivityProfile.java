f09726d50a63736fc6e42a972ae510cabac0cc7f
Nguyễn Hữu Tuấn <huutuan345679@gmail.com>
2020-08-26 21:12:09 +0700

    add api

--- app/src/main/java/com/example/cookdi/profile/ActivityProfile.java
+++ app/src/main/java/com/example/cookdi/profile/ActivityProfile.java
@@ -1,15 +1,27 @@
 package com.example.cookdi.profile;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.cookdi.R;
+import com.example.cookdi.retrofit2.ServiceManager;
+import com.example.cookdi.retrofit2.entities.RecipeDetail;
+import com.example.cookdi.retrofit2.entities.User;
+import com.example.cookdi.sharepref.SharePref;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ActivityProfile extends AppCompatActivity {
 
@@ -20,6 +32,8 @@ public class ActivityProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        fetchData();
 
         Changename = (Button) findViewById(R.id.Changenamebtn);
         Logout = (Button) findViewById(R.id.logoutbtn);
@@ -49,5 +63,33 @@ public class ActivityProfile extends AppCompatActivity {
         });
 
     }
+    private void fetchData(){
+        System.out.println("____________");
+        System.out.println("fech data");
+//        ServiceManager.getInstance().getUserService().getUserById(SharePref.getInstance(getApplicationContext()).getUuid()).enqueue(new Callback<User>() {
+//            @Override
+//            public void onResponse(Call<User> call, Response<User> response) {
+//                System.out.println("_________________");
+//                System.out.println(response.body().getName());
+//            }
+//
+//            @Override
+//            public void onFailure(Call<User> call, Throwable t) {
+//                System.out.println("_________________");
+//                t.printStackTrace();
+//            }
+//        });
+        ServiceManager.getInstance().getRecipeService().getAllRecipe(0).enqueue(new Callback<List<RecipeDetail>>() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onResponse(Call<List<RecipeDetail>> call, Response<List<RecipeDetail>> response) {
+                System.out.println("_________________1");
+            }
 
+            @Override
+            public void onFailure(Call<List<RecipeDetail>> call, Throwable t) {
+                System.out.println("_________________2");
+            }
+        });
+    }
 }
